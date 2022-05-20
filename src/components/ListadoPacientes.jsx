b718db9841d7f3fe479b4bbbcf6dd5cff1276959
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-05-20 00:20:20 -0600

    Se cambió titulo del listado

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -26,7 +26,7 @@ const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
         <>
           <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
           <p className="text-xl mt-5 mb-10 text-center font-semibold">
-            Comienza agreagando pacientes{" "}
+            Comienza agregando pacientes{" "}
             <span className="text-indigo-600 font-bold">
               y aparecerán aqui.
             </span>
